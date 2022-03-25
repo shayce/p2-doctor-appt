c8a3e1fec47bdef79f4c1dd60de04e3cf2e22bb8
Joseph <josephtecson18@gmail.com>
2022-03-25 12:55:41 -0700

    add josephs component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JosephsComponentComponent } from './josephs-component/josephs-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JosephsComponentComponent
   ],
   imports: [
     BrowserModule,
