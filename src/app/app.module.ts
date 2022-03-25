92a2e10ee8caa86ecb1eca7236b429aa5047d2cc
Kami Ruebush <kami691@revature.net>
2022-03-25 15:28:11 -0500

    Kamis commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { JosephsComponentComponent } from './josephs-component/josephs-component.component';
 import { TreyscomponentComponent } from './treyscomponent/treyscomponent.component';
 import { TreysComponentComponent } from './treys-component/treys-component.component';
+import { KamisComponent } from './kamis/kamis.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     JosephsComponentComponent,
     TreyscomponentComponent,
-    TreysComponentComponent
+    TreysComponentComponent,
+    KamisComponent
   ],
   imports: [
     BrowserModule,
